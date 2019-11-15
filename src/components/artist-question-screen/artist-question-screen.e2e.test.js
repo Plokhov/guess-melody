b8e7955a9d86a24dfb188b501cba2c3aed6f488c
Mikhail <plohov.mm@mail.ru>
2019-11-15 11:39:37 +0500

    Учебный проект. Раздел 3. State. C e2e тестами на отправку формы

--- src/components/artist-question-screen/artist-question-screen.e2e.test.js
+++ src/components/artist-question-screen/artist-question-screen.e2e.test.js
@@ -1,44 +1,38 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {GenreQuestionScreen} from './genre-question-screen.jsx';
+import {ArtistQuestionScreen} from './artist-question-screen.jsx';
 
 Enzyme.configure({adapter: new Adapter()});
 
 it(`When the user answers, the callback function gets the data in the correct format`, () => {
   const onUserAnswer = jest.fn();
-  const genreQuestionScreen = shallow(<GenreQuestionScreen
+  const genreQuestionScreen = shallow(<ArtistQuestionScreen
     screenIndex={0}
     question={{
       type: `a`,
-      genre: `rock`,
+      song: {
+        artist: `a`,
+        src: `a`,
+      },
       answers: [
         {
-          src: `a`,
-          genre: `a`,
+          picture: `a`,
+          artist: `a`,
         },
         {
-          src: `a`,
-          genre: `a`,
+          picture: `b`,
+          artist: `b`,
         },
         {
-          src: `a`,
-          genre: `a`,
-        },
-        {
-          src: `a`,
-          genre: `a`,
+          picture: `c`,
+          artist: `c`,
         },
       ],
     }}
     onAnswer={onUserAnswer}
   />);
 
-  genreQuestionScreen.find(`form`).simulate(`submit`, {
-    preventDefault: () => {}
-  });
-
+  genreQuestionScreen.find(`form`).simulate(`change`);
   expect(onUserAnswer).toHaveBeenCalledTimes(1);
 });
-
-
